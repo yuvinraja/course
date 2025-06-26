6e2142e20a31bd6340f448526673834a9b7d92ab
Yuvin Raja <yuvinrajav@gmail.com>
2025-06-26 15:37:15 +0530

    simple express server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-import http from "http";
+import app from "./server.js";
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ message: "hello" }));
-
-    res.end();
-    return;
-  }
-
-  res.writeHead(404, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "nope" }));
-});
-
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
